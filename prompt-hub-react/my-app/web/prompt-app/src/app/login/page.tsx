d4c3b7409ea8ce744ba22a5b4f2d6ec7a12f9a58
B-eunji <a369pej@gmail.com>
2025-01-15 20:52:01 +0900

    로그인을 위한 환경설정
    - Zustand 설치
    - store에 로그인 상태 관리 authStore 생성
    -page.tsx에 zustand 적용

--- prompt-hub-react/my-app/web/prompt-app/src/app/login/page.tsx
+++ prompt-hub-react/my-app/web/prompt-app/src/app/login/page.tsx
@@ -1,10 +1,19 @@
+"use client"; //클라이언트 상태 관리 - 클라이언트 컴포넌트 설정
+
 import AuthForm from "./AuthForm";
+import useAuthStore from "../../store/authStore"; //Zustand 상태 관리
 
 export default function LoginPage() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn); //로그인 상태 확인
+
   return (
     <div>
       <h1>로그인</h1>
-      <AuthForm />
+      {isLoggedIn ? (
+        <p> 로그인 완료 </p>
+      ) : (
+        <AuthForm />
+      )}
     </div>
   );
 }
---
+++ prompt-hub-react/my-app/web/prompt-app/src/store/authStore.ts
@@ -0,0 +1,20 @@
+//로그인 상태 관리
+import {create} from "zustand"; //상태 생성 함수
+
+//인터페이스 'AuthState' 정의: 상태의 구조를 타입으로 명시
+interface AuthState {
+    isLoggedIn:boolean; //로그인 상태 (T로그인, F로그아웃)
+    user: {id:string; name:string} | null; //유저 정보(id, 이름)
+    login: (user: {id: string, name: string}) => void; //로그인 함수
+    logout: () => void; //로그아웃 함수
+}
+
+//Zustand의 상태 관리 로직
+const useAuthStore = create<AuthState>((set) => ({
+    isLoggedIn: false, //초기 상태 = 로그아웃
+    user: null, //초기 유저정보 = null
+    login: (user) => set({isLoggedIn: true, user}), //로그인 시 상태 업데이트
+    logout: () => set({isLoggedIn:false, user:null}), //로그아웃 시 상태 초기화
+}));
+
+export default useAuthStore; //내보내기
